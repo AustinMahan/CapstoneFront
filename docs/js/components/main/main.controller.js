6821a5b835087c3789d324cb67828fd29d3076cc
AustinMahan <amahan0424@gmail.com>
2016-12-06 15:25:38 -0700

    polish

--- docs/js/components/main/main.controller.js
+++ docs/js/components/main/main.controller.js
@@ -10,26 +10,12 @@
 
   function mainController($http, $rootScope, $location) {
     /*jshint validthis: true */
-    var username, password;
     var vm = this
     vm.greeting = 'Hello World!';
-    vm.user = username = "ViperDriver-21";
-    vm.pass = password = 'password'
 
     vm.signup = function () {
       $location.path('/signup')
     }
-    // $http.post('https://obscure-hamlet-56226.herokuapp.com/psn', {username, password})
-    // .then(data => {
-    //   console.log(data);
-    //   if (data.data.error) vm.error = data.data.error
-    //   else {
-    //     $rootScope.games = split(data.data, 2)
-    //     $rootScope.user = data.data[0].fromUser.onlineId
-    //     $rootScope.userEmail = username
-    //     $location.path('/psn')
-    //   }
-    // })
 
     vm.button = true;
     vm.signInPsn = function(username, password){
